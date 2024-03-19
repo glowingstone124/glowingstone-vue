7bcdb63db63cadfbf3dc90b968a3dabf94c9b526
glowingstone124 <hanserofficial@outlook.com>
2024-03-19 19:33:30 +0800

    friends

--- src/components/Friends/Friends.vue
+++ src/components/Friends/Friends.vue
@@ -1,13 +1,55 @@
 <template>
-    <h1 class="contents">Friends</h1>
+  <div class="contents">
+    <h1 class="cagetory">Friends</h1>
+    <div class="friends">
+      <FriendsComponent
+        :avatar="'https://avatars.githubusercontent.com/u/80297629?v=4'"
+        :name="'glowingstone124'"
+        :description="'Java & web Dev java和web开发者'"
+        :link="'https://github.com'"
+      />
+      <FriendsComponent
+        :avatar="'https://gravatar.loli.net/avatar/0bf16c751d119f1fb5e76d2bdef47fd4'"
+        :name="'社会易姐QwQ'"
+        :description="''"
+        :link="'https://shakaianee.top/'"
+      />
+      <FriendsComponent
+        :avatar="'https://avatars.githubusercontent.com/u/80297629?v=4'"
+        :name="'glowingstone124'"
+        :description="'Java & web Dev java和web开发者'"
+        :link="'https://github.com'"
+      />
+      <FriendsComponent
+        :avatar="'https://avatars.githubusercontent.com/u/80297629?v=4'"
+        :name="'glowingstone124'"
+        :description="'Java & web Dev java和web开发者'"
+        :link="'https://github.com'"
+      />
+    </div>
+  </div>
   </template>
   <script>
   export default {
-    name: 'Friends'
-  }
+    name: 'Friends',
+    components: {
+        FriendsComponent
+      }
+    }
+  import FriendsComponent from './FriendsComponent.vue';
   </script>
   
   <style scoped>
   @import "@/css/main.css";
+  .contents{
+    margin-top: 6vh;
+  }
+  .cagetory{
+    font-size: 5em;
+    font-weight: 500;
+  }
+  .friends{
+    margin-top: 3vh;
+  }
   </style>
   
---
+++ src/components/Friends/FriendsComponent.vue
@@ -0,0 +1,63 @@
+<template>
+    <div @click="visitProfile" class="friend-container">
+        <div class="avatar-container">
+            <img :src="avatar" alt="Avatar">
+        </div>
+        <div class="info-container">
+            <h2>{{ name }}</h2>
+            <p>{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FriendsComponent',
+  props: {
+    avatar: String,
+    name: String,
+    description: String,
+    link: String
+  },
+  methods: {
+    visitProfile() {
+      if (this.link) {
+        window.location.href = this.link;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import "@/css/main.css";
+.friend-container {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  margin: 1vh;
+  background-color: #3d4b5c;
+  padding: 4vh;
+  max-width:65vw;
+  transition: background-color 0.1s;
+  border-radius: 25px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+.friend-container:hover {
+    background-color: #5f748f;
+}
+
+.avatar-container {
+  margin-right: 40px;
+}
+
+img {
+  max-width: 110px;
+  border-radius: 50%;
+}
+
+.info-container {
+  flex-grow: 1;
+}
+</style>
+
