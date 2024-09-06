ad186dcd631e272b61b4416f01034c0667c97eaf
glowingstone124 <hanserofficial@outlook.com>
2024-09-06 23:44:51 +0800

    new styles

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -2,43 +2,55 @@
 	<div class="content">
 		<h1 class="introduction">文章列表</h1>
 		<div class="article" v-for="article in articles" :key="article.id" @click="redirect(article.id)">
-			<img :src="`/articles/${article.id}.jpg`" alt="文章图片" class="article-image" />
-			<h2 class="artc">{{ article.title }}</h2>
-			<h2 class="abstract">{{ article.abstract }}</h2>
+			<img :src="getImagePath(article.id)" alt="文章图片" class="article-image"/>
+			<div class="article-info">
+				<h2 class="artc">{{ article.title }}</h2>
+				<h2 class="abstract">{{ article.abstract }}</h2>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import path from 'path'; // 引入path模块
+import path from 'path';
 
 export default {
-  name: 'ArticleList',
-  data() {
-    return {
-      articles: [],
-    };
-  },
-  async mounted() {
-    const files = require.context("@root/public/articles/", true, /\.md$/);
-    const fm = require('front-matter');
-    const filelist = files.keys();
+	name: 'ArticleList',
+	data() {
+		return {
+			articles: [],
+		};
+	},
+	async mounted() {
+		const files = require.context("@root/public/articles/", true, /\.md$/);
+		const fm = require('front-matter');
+		const filelist = files.keys();
 
-    for (const key of filelist) {
-      const data = fm(files(key));
-      const fileName = path.basename(key);
-      this.articles.push({
-        id: fileName,
-        title: data.attributes.title,
-        abstract: data.attributes.abstract,
-      });
-    }
-  },
-  methods: {
-    redirect(articleId) {
-      this.$router.push(`/article/${articleId}`);
-    },
-  },
+		for (const key of filelist) {
+			const data = fm(files(key));
+			const fileName = path.basename(key);
+			this.articles.push({
+				id: fileName,
+				title: data.attributes.title,
+				abstract: data.attributes.abstract,
+			});
+		}
+	},
+	methods: {
+		getImagePath(articleId) {
+			const imagePath = `/articles/${articleId}.jpg`;
+			const defaultImage = '/articles/default.jpg';
+			try {
+				require(`@/assets${imagePath}`);
+				return imagePath;
+			} catch (e) {
+				return defaultImage;
+			}
+		},
+		redirect(articleId) {
+			this.$router.push(`/article/${articleId}`);
+		},
+	},
 };
 </script>
 
@@ -46,7 +58,6 @@ export default {
 .article {
 	border: 2px solid #869cb6;
 	border-radius: 10px;
-	padding: 20px 50px;
 	max-width: 55vw;
 	margin: auto;
 	min-height: 20vh;
@@ -58,9 +69,7 @@ export default {
 .article-image {
 	width: 100%;
 	height: auto;
-	min-height: 200px;
-	border-top-left-radius: 28px;
-	border-top-right-radius: 28px;
+	max-height: 900px;
 	margin-bottom: 10px;
 }
 
@@ -68,11 +77,15 @@ export default {
 	background-color: #435060;
 	scale: 1.02;
 }
+
 .artc {
-	margin-top: 10px; /* 标题与图片的间距 */
+	margin-top: 10px;
 }
 
 .abstract {
-	margin-top: 5px; /* 简介与标题的间距 */
+	margin-top: 5px;
+}
+.article-info {
+	margin: 30px;
 }
 </style>
