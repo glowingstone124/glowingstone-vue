51a93f6371736f17dc6b590c3e73c5b23eb5fd80
glowingstone124 <hanserofficial@outlook.com>
2024-09-07 20:57:12 +0800

    new styles

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
 	<Nav/>
-  <div class="main-frame">
-  <router-view></router-view>
-  </div>
-  <Footer Footermsg="design & code by glowingstone124"/>
+	<div class="main-frame">
+		<router-view></router-view>
+	</div>
+	<Footer Footermsg="design & code by glowingstone124"/>
 </template>
 <script>
 import Nav from './components/Nav.vue';
 import Footer from './components/Footer.vue';
+
 export default {
-  name: 'App',
-  components: {
-    Nav,
-    Footer
-  },
+	name: 'App',
+	components: {
+		Nav,
+		Footer
+	},
 }
 </script>
 <style scoped>
 @import "@/css/main.css";
+
 .main-frame {
-  padding-top:30px;
+	padding-top: 30px;
 	max-width: 100vw;
 }
 </style>
