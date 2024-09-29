17929b48e51c10468fbb0fb561f539f34d779ef2
glowingstone124 <hanserofficial@outlook.com>
2024-09-29 16:14:50 +0800

    done

--- src/components/About.vue
+++ src/components/About.vue
@@ -51,7 +51,7 @@ export default {
 		}
 	},
 	mounted() {
-		document.body.style.backgroundImage = 'url("/komeji2.jpg")';
+		document.body.style.backgroundImage = 'url("https://rock.glowingstone.cn/storage/download/komeji2.jpg")';
 		document.body.style.backgroundSize = 'cover';
 		document.body.style.backgroundRepeat = 'no-repeat'
 		document.body.style.backgroundAttachment = 'fixed';
