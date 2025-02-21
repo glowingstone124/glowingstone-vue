2c614c824afc21d33114e8e6f40792f4801b29ca
glowingstone124 <hanserofficial@outlook.com>
2025-02-21 18:33:30 +0800

    attempting changing new visit counter on cloudflare worker

--- src/components/VisitCounter.vue
+++ src/components/VisitCounter.vue
@@ -5,10 +5,7 @@ let visitCount = ref(0);
 
 async function incrementVisitCount(){
 	try {
-		const response = await fetch("https://lenient-dove-46659.upstash.io/get/visitcount", {
-			headers: {
-				Authorization: "Bearer AbZDASQgYTZmZGM5MTctZjYyNi00YzdjLWE3ZWUtZWQyZjVhNzBhMzA0ZmNjZjMzZDRkMWFiNGYwYmIwMDQ2Yjg1NWNhMmJmYzU=" // 请替换为你的实际授权令牌
-			}
+		const response = await fetch("https://misc.glowingstone.cn/increment", {
 		});
 
 		if (!response.ok) {
@@ -17,25 +14,10 @@ async function incrementVisitCount(){
 
 		const data = await response.json();
 		const resultData = JSON.parse(data.result);
-		visitCount.value = resultData.value; // 更新ref的值
+		visitCount.value = resultData.value;
 
 		console.log("Visit count:", visitCount.value);
 		visitCount.value++;
-
-		const updateResponse = await fetch("https://lenient-dove-46659.upstash.io/set/visitcount", {
-			method: "POST",
-			headers: {
-				Authorization: "Bearer AbZDASQgYTZmZGM5MTctZjYyNi00YzdjLWE3ZWUtZWQyZjVhNzBhMzA0ZmNjZjMzZDRkMWFiNGYwYmIwMDQ2Yjg1NWNhMmJmYzU=", // 请替换为你的实际授权令牌
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				value: visitCount.value
-			})
-		});
-
-		if (!updateResponse.ok) {
-			throw new Error(`Failed to update visit count: ${updateResponse.statusText}`);
-		}
 
 		console.log("Updated visit count:", visitCount.value);
 	} catch (error) {
@@ -43,7 +25,22 @@ async function incrementVisitCount(){
 	}
 }
 
+async function fetchVisitCount(){
+	try {
+		const response = await fetch("https://misc.glowingstone.cn/count", {})
+		if (!response.ok) {
+			throw new Error(`Failed to fetch visit count: ${response.statusText}`);
+		}
+		const data = await response.json();
+		visitCount.value = data.visit_count;
+
+	} catch (error) {
+		console.error("Error:", error);
+	}
+}
+
 onMounted(() => {
+	fetchVisitCount();
 	incrementVisitCount();
 });
 </script>
